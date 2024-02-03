6557c097f5e516f764017ce6c6c04b40007c5f32
Anton Nasonov <anton.nasonov1992@gmail.com>
2024-02-03 20:27:38 +0300

    added allowable network subdomains to exceptions

--- shared/src/commonMain/kotlin/com/example/blackcardskmm/util/Constants.kt
+++ shared/src/commonMain/kotlin/com/example/blackcardskmm/util/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.blackcardskmm.util
 
 object Constants {
+    const val BASE_HOST = "192.168.0.190"
     const val BASE_URL = "http://192.168.0.190:8080"
     const val USER_MOCKS_URL = "https://randomuser.me/api/"
     const val IMAGE_MOCKS_URL = "https://picsum.photos"
